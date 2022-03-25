4dc7fa66f8d25575d374df85b6ceb04cf2d400b8
Akhilesh ketkar <ketkarakhilesh@gmail.com>
2022-03-26 01:12:34 +0530

    feat: text editor added

--- client/src/Component/Dashboard.js
+++ client/src/Component/Dashboard.js
@@ -2,12 +2,18 @@ import moment from "moment";
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import socketio from "socket.io-client";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import sendArrow from "../Assets/arrow.PNG"
 
 function Dashboard() {
   const { name, roomId } = useParams();
   const message = useRef("");
   const [socket, setsocket] = useState(null);
   const [messages, setmessages] = useState([]);
+  const [editorState, seteditorState] = useState("");
 
   useEffect(() => {
     let socket = socketio(`http://${window.location.hostname}:4000`);
@@ -27,7 +33,8 @@ function Dashboard() {
   }, [socket]);
 
   const sendMessage = () => {
-    socket.emit("chatMessage", message.current.value);
+    let data = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+    socket.emit("chatMessage", data);
   };
 
   return (
@@ -42,15 +49,62 @@ function Dashboard() {
                 <div className='message' key={idx}>
                   <div className='user'>{item.user}</div>
                   <div className='message-details'>
-                    <div className='message'>{item.message}</div>
+                    <div className='message-text'>
+                      {
+                        <div
+                          dangerouslySetInnerHTML={{ __html: item.message }}
+                        />
+                      }
+                    </div>
                     <div className='date'>{moment(item.date).fromNow()}</div>
                   </div>
                 </div>
               ))}
             </div>
-            <div className='editor'>
-              <input type='text' ref={message} />
-              <button onClick={sendMessage}>Send</button>
+            <div className='editor-div-chat'>
+              <Editor
+                editorState={editorState}
+                onEditorStateChange={(e) => seteditorState(e)}
+                placeholder='Chat comes here'
+                wrapperClassName='demo-wrapper'
+                editorClassName='demo-editor'
+                toolbar={{
+                  options: ["inline", "link", "list", "emoji", "image"],
+                  inline: {
+                    inDropdown: false,
+                    className: undefined,
+                    component: undefined,
+                    dropdownClassName: undefined,
+                    options: ["bold", "italic", "strikethrough", "monospace"],
+                  },
+                  list: {
+                    inDropdown: false,
+                    className: undefined,
+                    component: undefined,
+                    dropdownClassName: undefined,
+                    options: ["ordered", "unordered", "indent"],
+                  },
+                }}
+                mention={{
+                  separator: " ",
+                  trigger: "@",
+                  suggestions: [
+                    { text: "APPLE", value: "apple", url: "apple" },
+                    { text: "BANANA", value: "banana", url: "banana" },
+                    { text: "CHERRY", value: "cherry", url: "cherry" },
+                    { text: "DURIAN", value: "durian", url: "durian" },
+                    { text: "EGGFRUIT", value: "eggfruit", url: "eggfruit" },
+                    { text: "FIG", value: "fig", url: "fig" },
+                    {
+                      text: "GRAPEFRUIT",
+                      value: "grapefruit",
+                      url: "grapefruit",
+                    },
+                    { text: "HONEYDEW", value: "honeydew", url: "honeydew" },
+                  ],
+                }}
+              />
+              <button onClick={sendMessage}><img src={sendArrow} alt="send-arrow" /></button>
             </div>
           </div>
         </div>
